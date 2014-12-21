d88b7ce698ac3b4ac1a45d3538324e79738e0911
Jon Carlisle <joncarlisle2017@u.northwestern.edu>
2014-12-20 22:22:41 -0600

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## matrix is passed in and a list is created with getters and setters.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  
+  ## initialize x to value passed in and m to NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ## retrieve x
+  get <- function() x
+  
+  ## cache m to value passed in
+  setcache <- function(solve) m <<- solve
+  
+  ## retrieve m
+  getcache <- function() m
+  
+  ## return a list of functions
+  list(set = set, get = get,
+       setcache = setcache,
+       getcache = getcache)
 }
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ##call getcache function from the list of functions passed in
+  m <- x$getcache()
+  
+  ##if m is not null retrieve m from cache and tell the user such
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  ## m is null so call the get function from list of functions passed into cacheSolve()
+  ## data will become the special matrix 
+  data <- x$get()
+  
+  ##compute the inverse of the special matrix
+  m <- solve(data, ...)
+  
+  ## the special matrix is cached 
+  x$setcache(m)
+  
+  ##return the inverse of the special matrix
+  m
 }
